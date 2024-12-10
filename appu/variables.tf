79e4b985e2a5b8df2519327073c53ca0f669a14c
Rahulravib <rahul.ravi@bootlabstech.com>
2024-12-10 17:45:39 +0530

    Terraform pipeline 1

---
+++ appu/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_name" {}
+variable "vpc_cidr_block" {}
+variable "subnet1_cidr_block" {}
+variable "subnet1_availabity_zone" {}
+variable "subnet1_name" {}
+variable "subnet2_cidr_block" {}
+variable "subnet2_availabity_zone" {}
+variable "subnet2_name" {}
+variable "subnet3_cidr_block" {}
+variable "subnet3_availabity_zone" {}
+variable "subnet3_name" {}
+variable "vpc1_internet_gw" {}
+variable "security_group_name" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "bucket_count" { }
+
+
+
